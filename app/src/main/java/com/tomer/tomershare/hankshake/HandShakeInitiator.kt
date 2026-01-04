939ca092e12d28a0ec77a6092a5803d8d8379ec6
tomer00 <himanshutomer00@gmail.com>
2026-01-04 12:41:40 +0530

    HandShake Classes

--- app/src/main/java/com/tomer/tomershare/hankshake/HandShakeInitiator.kt
+++ app/src/main/java/com/tomer/tomershare/hankshake/HandShakeInitiator.kt
@@ -1,4 +1,55 @@
 package com.tomer.tomershare.hankshake
 
-class HandShakeInitiator {
+import android.util.Log
+import com.tomer.tomershare.utils.Utils
+import com.tomer.tomershare.utils.Utils.Companion.longFromBytearray
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import java.net.Socket
+import java.nio.charset.StandardCharsets
+import kotlin.concurrent.thread
+
+class HandShakeInitiator(
+    private val onReceive: (String, String, String) -> Unit
+) {
+    private var soc: Socket? = null
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    fun initFindingIp(ip: String) {
+        Log.d("TAG--", "initFindingIp: $ip")
+        val sts = ip.split(".")
+        if (sts.size != 4) {
+            clean()
+            return
+        }
+        thread(start = true, isDaemon = true) {
+            for (i in 1..255) {
+                scope.launch {
+                    val newIp = "${sts[0]}.${sts[1]}.${sts[2]}.$i"
+                    try {
+                        soc = Socket(newIp, Utils.Companion.HAND_SHAKE_PORT)
+                        val sizeBytes = ByteArray(8)
+                        soc?.inputStream?.read(sizeBytes)
+                        val s = sizeBytes.longFromBytearray()
+                        val nameBytes = ByteArray(s.toInt())
+                        soc?.inputStream?.read(nameBytes)
+                        val name = String(nameBytes, StandardCharsets.UTF_8)
+                        onReceive(newIp, name.substring(1), name.substring(0, 1))
+                        clean()
+                        scope.cancel()
+                    } catch (_: Exception) {
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun clean() {
+        runCatching { soc?.close() }
+        soc = null
+    }
 }
--- app/src/main/java/com/tomer/tomershare/hankshake/HandShakeServer.kt
+++ app/src/main/java/com/tomer/tomershare/hankshake/HandShakeServer.kt
@@ -1,2 +1,33 @@
-package com.tomer.tomershare.hankshake 
+package com.tomer.tomershare.hankshake
 
+import com.tomer.tomershare.utils.Utils
+import com.tomer.tomershare.utils.Utils.Companion.bytesFromLong
+import java.net.ServerSocket
+import kotlin.concurrent.thread
+
+class HandShakeServer() {
+
+    private val serverSocket = ServerSocket(Utils.Companion.HAND_SHAKE_PORT)
+    private var isActive = true
+
+    init {
+        thread(start = true, isDaemon = true) {
+            while (isActive) {
+                try {
+                    val soc =serverSocket.accept()
+                    val d = "${Utils.Companion.myIcon}${Utils.Companion.myName}"
+                    soc.outputStream.write(d.length.toLong().bytesFromLong())
+                    soc.outputStream.write(d.toByteArray(Charsets.UTF_8))
+                } catch (_: Exception) {
+                }
+            }
+        }
+    }
+
+    fun stop() {
+        isActive = false
+        runCatching {
+            serverSocket.close()
+        }
+    }
+}
